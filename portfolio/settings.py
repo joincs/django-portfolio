5f753fa0b268ddae59c1a8c248610a289b0f8525
joincs <68950671+joincs@users.noreply.github.com>
2020-08-12 15:55:34 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'codingspark786@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bbf9ec88'
+EMAIL_HOST_USER = 'youremail@example.com'
+EMAIL_HOST_PASSWORD = 'your email password'
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
